e95c86aece428221b4dcd7f0cf06bfa09ebd8ac0
xdecker <xavier_deckza@hotmail.com>
2026-02-07 21:03:27 -0500

    feat: prisma client service configuration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,18 @@
+import 'dotenv/config';
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaBetterSqlite3({
+      url: process.env.DATABASE_URL!,
+    });
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
