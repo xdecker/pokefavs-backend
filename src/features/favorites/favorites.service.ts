d7a696f9fd2af3a591732616261d28976b335445
xdecker <xavier_deckza@hotmail.com>
2026-02-07 21:04:03 -0500

    feat(favorites): endpoint of post and get

--- src/features/favorites/favorites.service.ts
+++ src/features/favorites/favorites.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { generateCode } from 'src/common/utils/code-generator';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(pokemons: string[]) {
+    const code = await this.generateUniqueCode();
+
+    if (pokemons.length == 0) {
+      throw new BadRequestException("pokemons favorite list can't be empty");
+    }
+
+    //you can only have 6 slots of pokemons in games so..
+    if (pokemons.length > 6) {
+      throw new BadRequestException('max 6 pokemons allowed');
+    }
+
+    const unique = new Set(pokemons);
+
+    if (unique.size !== pokemons.length) {
+      throw new BadRequestException('Duplicate Pokemons list not allowed');
+    }
+
+    const list = await this.prisma.favoriteList.create({
+      data: {
+        code,
+        pokemons,
+      },
+    });
+
+    return { code: list.code };
+  }
+
+  async findByCode(code: string) {
+    if(!code) throw new BadRequestException("Please provide a valid code of the list");
+    const list = await  this.prisma.favoriteList.findUnique({
+      where: { code },
+    });
+    if(!list){
+        throw new NotFoundException("favorite list not found");
+    }
+
+    return list;
+  }
+
+  private async generateUniqueCode(): Promise<string> {
+    let code: string;
+    let exists: boolean;
+
+    do {
+      code = generateCode();
+
+      const list = await this.prisma.favoriteList.findUnique({
+        where: { code },
+      });
+
+      exists = !!list;
+    } while (exists);
+
+    return code;
+  }
+}
